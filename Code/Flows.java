09cbc52670b942032212cb66c6354c54abc507c9
FilipPieniazek <116604247+FilipPieniazek@users.noreply.github.com>
2024-01-18 02:35:43 +0100

    Flows.java code cleanup

--- Code/Flows.java
+++ Code/Flows.java
@@ -1,293 +1,157 @@
 package pl.edu.agh.kt;
 
-
-
 import java.util.ArrayList;
-
 import java.util.Collections;
-
 import java.util.List;
-
 import java.util.Map;
 
-
-
 import org.projectfloodlight.openflow.protocol.OFFlowDelete;
-
 import org.projectfloodlight.openflow.protocol.OFFlowMod;
-
 import org.projectfloodlight.openflow.protocol.OFPacketIn;
-
 import org.projectfloodlight.openflow.protocol.OFPacketOut;
-
 import org.projectfloodlight.openflow.protocol.action.OFAction;
-
 import org.projectfloodlight.openflow.protocol.action.OFActionOutput;
-
 import org.projectfloodlight.openflow.protocol.match.Match;
-
 import org.projectfloodlight.openflow.protocol.match.MatchField;
-
 import org.projectfloodlight.openflow.types.EthType;
-
 import org.projectfloodlight.openflow.types.IPv4Address;
-
 import org.projectfloodlight.openflow.types.IpProtocol;
-
 import org.projectfloodlight.openflow.types.MacAddress;
-
 import org.projectfloodlight.openflow.types.OFPort;
-
 import org.projectfloodlight.openflow.types.OFVlanVidMatch;
-
 import org.projectfloodlight.openflow.types.TransportPort;
-
 import org.projectfloodlight.openflow.types.VlanVid;
 
 import org.slf4j.Logger;
-
 import org.slf4j.LoggerFactory;
 
-
-
 import net.floodlightcontroller.core.FloodlightContext;
-
 import net.floodlightcontroller.core.IFloodlightProviderService;
-
 import net.floodlightcontroller.core.IOFSwitch;
-
 import net.floodlightcontroller.packet.Data;
-
 import net.floodlightcontroller.packet.Ethernet;
-
 import net.floodlightcontroller.packet.IPv4;
-
 import net.floodlightcontroller.packet.TCP;
-
 import net.floodlightcontroller.packet.UDP;
+import net.floodlightcontroller.statistics.StatisticsCollector;
 
 
 
 public class Flows {
-
-
-
 	private static final Logger logger = LoggerFactory.getLogger(Flows.class);
 
-
-
 	public static short FLOWMOD_DEFAULT_IDLE_TIMEOUT = 5; // in seconds
-
 	public static short FLOWMOD_DEFAULT_HARD_TIMEOUT = 0; // infinite
-
 	public static short FLOWMOD_DEFAULT_PRIORITY = 100;
 
-
-
 	protected static boolean FLOWMOD_DEFAULT_MATCH_VLAN = true;
-
 	protected static boolean FLOWMOD_DEFAULT_MATCH_MAC = true;
-
 	protected static boolean FLOWMOD_DEFAULT_MATCH_IP_ADDR = true;
-
 	protected static boolean FLOWMOD_DEFAULT_MATCH_TRANSPORT = true;
 
-
-
 	public Flows() {
-
 		logger.info("Flows() begin/end");
-
 	}
-
-
 
 	public static void simpleAdd(IOFSwitch sw, OFPacketIn pin, FloodlightContext cntx, OFPort outPort) {
-
-		
-
 		PacketExtractor extractor = new PacketExtractor(cntx);
-
 		String sourceMac = extractor.getSourceMac().toString();
 
-		
+//		Map<String, Integer> blackList = StatisticsCollector.blackList;
 
-		Map<String, Integer> blackList = StatisticsCollector.blackList;
+		// FlowModBuilder
+		OFFlowMod.Builder fmb = sw.getOFFactory().buildFlowAdd();
 
-		
+		// match
+		Match m = createMatchFromPacket(sw, pin.getInPort(), cntx);
 
-		if (!blackList.containsKey(sourceMac)) {	
+		// actions
+		OFActionOutput.Builder aob = sw.getOFFactory().actions().buildOutput();
 
-			// FlowModBuilder
+		List<OFAction> actions = new ArrayList<OFAction>();
 
-			OFFlowMod.Builder fmb = sw.getOFFactory().buildFlowAdd();
+		aob.setPort(outPort);
+		aob.setMaxLen(Integer.MAX_VALUE);
+		actions.add(aob.build());
+		fmb.setMatch(m).setIdleTimeout(FLOWMOD_DEFAULT_IDLE_TIMEOUT).setHardTimeout(FLOWMOD_DEFAULT_HARD_TIMEOUT)
+				.setBufferId(pin.getBufferId()).setOutPort(outPort).setPriority(FLOWMOD_DEFAULT_PRIORITY);
 
-			// match
+		fmb.setActions(actions);
 
-			Match m = createMatchFromPacket(sw, pin.getInPort(), cntx);
-
-	
-
-			// actions
-
-			OFActionOutput.Builder aob = sw.getOFFactory().actions().buildOutput();
-
-			List<OFAction> actions = new ArrayList<OFAction>();
-
-			aob.setPort(outPort);
-
-			aob.setMaxLen(Integer.MAX_VALUE);
-
-			actions.add(aob.build());
-
-			fmb.setMatch(m).setIdleTimeout(FLOWMOD_DEFAULT_IDLE_TIMEOUT).setHardTimeout(FLOWMOD_DEFAULT_HARD_TIMEOUT)
-
-					.setBufferId(pin.getBufferId()).setOutPort(outPort).setPriority(FLOWMOD_DEFAULT_PRIORITY);
-
-			fmb.setActions(actions);
-
-			// write flow to switch
-
-			try {
-
-				sw.write(fmb.build());
-
+		// write flow to switch
+		try {
+			sw.write(fmb.build());
 //				logger.info("Flow from port {} forwarded to port {}; match: {}", new Object[] { pin.getInPort().getPortNumber(), outPort.getPortNumber(), m.toString() });
-
-			} catch (Exception e) {
-
-				logger.error("error {}", e);
-
-			}
-
+		} catch (Exception e) {
+			logger.error("error {}", e);
 		}
-
 	}
 
-	
+	public static void deleteFlow(IOFSwitch sw, MacAddress srcMac) {
 
-    public static void deleteFlow(IOFSwitch sw, MacAddress srcMac) {
+		// Create a flow delete message
+		Match.Builder mb = sw.getOFFactory().buildMatch();
+		mb.setExact(MatchField.ETH_SRC, srcMac);
+		Match match = mb.build();
 
-        // Create a flow delete message
+		OFFlowDelete.Builder flowDeleteBuilder = sw.getOFFactory().buildFlowDelete();
+		flowDeleteBuilder.setMatch(match);
+		flowDeleteBuilder.setOutPort(OFPort.ANY);  // Set to any port for wildcard match
 
-        Match.Builder mb = sw.getOFFactory().buildMatch();
+		// Send the flow delete message to the switch
+		try {
+			sw.write(flowDeleteBuilder.build());
+			logger.info("Flow matching {} deleted.", match.toString());
+		} catch (Exception e) {
+			logger.error("Error deleting flow: {}", e.getMessage());
+		}
+	}
 
-        mb.setExact(MatchField.ETH_SRC, srcMac);
+	public static Match createMatchFromSourceMac(IOFSwitch sw, MacAddress srcMac) {
+		Match.Builder mb = sw.getOFFactory().buildMatch();
+		mb.setExact(MatchField.ETH_SRC, srcMac);
 
-        Match match = mb.build();
-
-        
-
-        OFFlowDelete.Builder flowDeleteBuilder = sw.getOFFactory().buildFlowDelete();
-
-        flowDeleteBuilder.setMatch(match);
-
-        flowDeleteBuilder.setOutPort(OFPort.ANY);  // Set to any port for wildcard match
-
-
-
-        // Send the flow delete message to the switch
-
-        try {
-
-            sw.write(flowDeleteBuilder.build());
-
-            logger.info("Flow matching {} deleted.", match.toString());
-
-        } catch (Exception e) {
-
-            logger.error("Error deleting flow: {}", e.getMessage());
-
-        }
-
-    }
-
-    
-
-    public static Match createMatchFromSourceMac(IOFSwitch sw, MacAddress srcMac) {
-
-        Match.Builder mb = sw.getOFFactory().buildMatch();
-
-        mb.setExact(MatchField.ETH_SRC, srcMac);
-
-
-
-        return mb.build();
-
-    }
-
-
+		return mb.build();
+	}
 
 	public static Match createMatchFromPacket(IOFSwitch sw, OFPort inPort, FloodlightContext cntx) {
 
 		// The packet in match will only contain the port number.
-
 		// We need to add in specifics for the hosts we're routing between.
-
 		Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-
 		VlanVid vlan = VlanVid.ofVlan(eth.getVlanID());
-
 		MacAddress srcMac = eth.getSourceMACAddress();
-
 		MacAddress dstMac = eth.getDestinationMACAddress();
-
-
 
 		Match.Builder mb = sw.getOFFactory().buildMatch();
 
 		mb.setExact(MatchField.IN_PORT, inPort);
 
-
-
 		if (FLOWMOD_DEFAULT_MATCH_MAC) {
-
 			mb.setExact(MatchField.ETH_SRC, srcMac).setExact(MatchField.ETH_DST, dstMac);
-
 		}
-
-
 
 		if (FLOWMOD_DEFAULT_MATCH_VLAN) {
-
 			if (!vlan.equals(VlanVid.ZERO)) {
-
 				mb.setExact(MatchField.VLAN_VID, OFVlanVidMatch.ofVlanVid(vlan));
-
 			}
-
 		}
 
-
-
 		// TODO Detect switch type and match to create hardware-implemented flow
-
 		if (eth.getEtherType() == EthType.IPv4) { /*
+		 * shallow check for equality is okay for EthType
 
-													 * shallow check for equality is okay for EthType
-
-													 */
-
+		 */
 			IPv4 ip = (IPv4) eth.getPayload();
-
 			IPv4Address srcIp = ip.getSourceAddress();
-
 			IPv4Address dstIp = ip.getDestinationAddress();
 
-
-
 			if (FLOWMOD_DEFAULT_MATCH_IP_ADDR) {
-
 				mb.setExact(MatchField.ETH_TYPE, EthType.IPv4).setExact(MatchField.IPV4_SRC, srcIp)
-
 						.setExact(MatchField.IPV4_DST, dstIp);
-
 			}
 
-
-
 			if (FLOWMOD_DEFAULT_MATCH_TRANSPORT) {
-
 				/*
 
 				 * Take care of the ethertype if not included earlier, since it's a prerequisite
@@ -295,50 +159,30 @@ public class Flows {
 				 * for transport ports.
 
 				 */
-
 				if (!FLOWMOD_DEFAULT_MATCH_IP_ADDR) {
-
 					mb.setExact(MatchField.ETH_TYPE, EthType.IPv4);
-
 				}
-
-
 
 				if (ip.getProtocol().equals(IpProtocol.TCP)) {
-
 					TCP tcp = (TCP) ip.getPayload();
-
 					mb.setExact(MatchField.IP_PROTO, IpProtocol.TCP).setExact(MatchField.TCP_SRC, tcp.getSourcePort())
-
 							.setExact(MatchField.TCP_DST, tcp.getDestinationPort());
-
-				} else if (ip.getProtocol().equals(IpProtocol.UDP)) {
-
+				} 
+				else if (ip.getProtocol().equals(IpProtocol.UDP)) {
 					UDP udp = (UDP) ip.getPayload();
-
 					mb.setExact(MatchField.IP_PROTO, IpProtocol.UDP).setExact(MatchField.UDP_SRC, udp.getSourcePort())
-
 							.setExact(MatchField.UDP_DST, udp.getDestinationPort());
-
 				}
-
 			}
+		} 
+		else if (eth.getEtherType() == EthType.ARP) { /*
 
-		} else if (eth.getEtherType() == EthType.ARP) { /*
+		 * shallow check for equality is okay for EthType
 
-														 * shallow check for equality is okay for EthType
-
-														 */
-
+		 */
 			mb.setExact(MatchField.ETH_TYPE, EthType.ARP);
-
 		}
 
-
-
 		return mb.build();
-
 	}
-
 }
-
